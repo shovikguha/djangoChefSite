a2785f730caee0955a2a1d68127625011c48c246
shovikguha <shovik.guha@gmail.com>
2018-06-12 22:59:03 -0700

    progress so far

--- chefsite/chefsite/settings.py
+++ chefsite/chefsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'files'
 ]
 
 MIDDLEWARE = [
@@ -73,11 +74,22 @@ WSGI_APPLICATION = 'chefsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+            'ENGINE' : 'django.db.backends.mysql',
+            'NAME' : 'chef_file_db',
+            'USER' : 'root',
+            'PASSWORD' : #replace with own password,
+            'HOST' : 'localhost',
+            'PORT': ''
+        }
 }
 
 
